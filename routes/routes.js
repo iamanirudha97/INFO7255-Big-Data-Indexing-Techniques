1ec87591c53676f449bf76c2c5023c2292497428
iamanirudha97 <sagarlm10@gmail.com>
2024-06-08 11:17:18 -0400

    implemented-getall-endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { savePlan, getPlanById, deletePlanByID } = require('../controllers/crud');
+const { savePlan, getPlanById, deletePlanByID, getAllPlans } = require('../controllers/crud');
 
 const appRouter = express.Router();
 
 appRouter.post("/v1/set/plan", savePlan)
 appRouter.get("/v1/get/plan/:planid", getPlanById)
 appRouter.delete("/v1/del/plan/:planid", deletePlanByID)
+appRouter.get("/v1/get/plans", getAllPlans)
 
 module.exports = appRouter
